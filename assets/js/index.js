1d3e69da585b73969f3a73a4afbc8909fba27fcd
shi3719 <shi3719@naver.com>
2024-06-14 02:36:14 +0900

    second

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,8 @@
-const arrows = document.querySelectorAll('.arow')
+const arrows = document.querySelectorAll('.arrow');
+
+arrows.forEach((arrow) => {
+    arrow.addEventListener('click', () => {
+        arrow.parentElement.parentElement.classList.toggle('on');
+        arrow.parentElement.parentElement.classList.toggle('off');
+    })
+})
